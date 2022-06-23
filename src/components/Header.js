9c3c482260c3e6fecb47f64eb4fa2e583a3283e6
BTorena <bautistatorena@gmail.com>
2022-06-23 01:33:34 -0300

    React task tracker

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import React from "react";
+import PropTypes from "prop-types";
+import Button from "./Button";
 
 const Header = ({ title }) => {
   return (
-    <header>
+    <header className="header">
       <h1>{title}</h1>
+      <Button />
     </header>
   );
 };
@@ -12,4 +15,12 @@ Header.defaultProps = {
   title: "Task Tracker",
 };
 
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+//CSS in JS
+// const headingStyle = {
+//   color: "red",
+//   backgroundColor: "black",
+// };
 export default Header;
